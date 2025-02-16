308e812fc5a8fd4cbee4c44357441965621c5aee
njnambiar <67516166+njnambiar@users.noreply.github.com>
2025-02-16 14:00:20 -0800

    Update and rename createWorkout.jsx to logWorkout.jsx

--- frontend/src/logWorkout.jsx
+++ frontend/src/logWorkout.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./index.css";
 import { useNavigate } from "react-router-dom";
 
-export default function Workout() {
+export default function logWorkout() {
   return (
     
     )

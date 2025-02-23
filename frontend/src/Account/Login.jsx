b5aa24dd5f56a0f2c66dedd9b3d136c65a8d5d0f
njnambiar <67516166+njnambiar@users.noreply.github.com>
2025-02-22 22:42:22 -0800

    Update Login.jsx

--- frontend/src/Account/Login.jsx
+++ frontend/src/Account/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { router } from './user.js'
 import "./accounts.css";
 import { signInApi } from "../api/api";
 
@@ -27,12 +26,6 @@ export default function Login() {
       console.log(error);
     }
   };
-=======
-  function login() {
-    const data = { username: String(username), password: String(password) };
-    router.post(data);
-    navigate("/");
-  }
 
   return (
     <div>

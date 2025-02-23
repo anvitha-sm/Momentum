5a8eb1962674a4a3e2317876b1922faa38ae5965
Anvitha Mattapalli <anvitha.mattapalli@gmail.com>
2025-02-23 14:05:06 -0800

    Friends Feature -- Backend
    Added friends component to the user schema to hold all of the user's
    friends. These are one-way relationships (like followers). APIs for
    adding and removing friends are completed.

---
+++ backend/src/routes/friends.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const User = mongoose.model("user");
+const authenticate = require("../middleware/authenticate");
+
+router.post("/api/friends/:friendId", authenticate, async (req, res) => {
+    const user = req.user;
+    const friendId = req.body;
+    try {
+        if (user._id == friendId) {
+            return res.status(400).json({ message: "Cannot friend yourself."});
+        }
+
+        const friend = await User.findById(friendId);
+        if (!friend) {
+            return res.status(404).json({ message: "Friend not found."});
+        }
+
+        if(user.friends.includes(friendId)) {
+            return res.status(400).json({ message: "Already friends."});
+        }
+        user.friends.push(friendId);
+        await user.save();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+router.delete("/api/friends/:friendId", authenticate, async (req, res) => {
+    const user = req.user;
+    const friendId = req.body;
+    try {
+        if (!user.friends.includes(friendId)) {
+            return res.status(400).json({ message: "Not previously friends." });
+        }
+
+        user.friends = user.friends.filter(id => id.toString() != friendId)
+        await user.save();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
